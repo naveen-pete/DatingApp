e24cfea1db954e3928ae4ee42d428a998e0ba200
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-12-11 16:22:59 +0530

    add alertify.js, angular jwt, ngx bootstrap and bootswatch third party libraries to the app

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,29 +11,28 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       () => {
-        console.log('Login successful!');
+        this.alertify.success('Login successful!');
       },
       error => {
-        console.log('Login failed!');
-        console.log('  Error:', error);
+        this.alertify.error('Login failed!');
+        console.log('Error:', error);
       }
     );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Logout successful.');
+    this.alertify.message('Logout successful.');
   }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,18 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('Registration successful!');
+        this.alertify.success('Registration successful!');
       },
       error => {
-        console.log('Registration failed!');
-        console.log('  Error:', error);
+        this.alertify.error('Registration failed!');
+        console.log('Error:', error);
       }
     );
   }
